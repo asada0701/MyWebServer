52b40a5c31146667359decfc4aded62c3a33ddb1
asada0701 <yusuke.nakashima@topgate.co.jp>
2017-04-19 13:01:40 +0900

    全てのテストを破棄

--- src/test/java/jp/co/topgate/asada/web/RequestMessageTest.java
+++ src/test/java/jp/co/topgate/asada/web/RequestMessageTest.java
@@ -15,145 +15,145 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by yusuke-pc on 2017/04/13.
  */
-@RunWith(Enclosed.class)
+//@RunWith(Enclosed.class)
 public class RequestMessageTest {
-    public static class methodのテスト{
-        jp.co.topgate.asada.web.RequestMessage sut = new RequestMessage();
-        @Before
-        public void setUp () {
-            sut = new RequestMessage();
-        }
-        @Test
-        public void nullチェック () {
-            assertThat(sut.getMethod(), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            sut.setMethod("GET");
-            assertThat(sut.getMethod(), is("GET"));
-        }
-    }
-
-    public static class URIのテスト{
-        RequestMessage sut;
-        @Before
-        public void setUp () {
-            sut = new RequestMessage();
-        }
-        @Test
-        public void nullチェック () {
-            assertThat(sut.getUri(), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            sut.setUri("/index.html");
-            assertThat(sut.getUri(), is("/index.html"));
-        }
-    }
-
-    public static class URIのクエリーのテスト{
-        RequestMessage sut;
-        @Before
-        public void setUp () {
-            sut = new RequestMessage();
-        }
-        @Test
-        public void nullチェック () {
-            assertThat(sut.findUriQuery(null), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            sut.setUriQuery("person", "人");
-            assertThat(sut.findUriQuery("person"), is("人"));
-        }
-    }
-
-    public static class プロトコルバージョンのテスト{
-        RequestMessage sut;
-        @Before
-        public void setUp () {
-            sut = new RequestMessage();
-        }
-        @Test
-        public void nullチェック () {
-            assertThat(sut.getProtocolVersion(), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            sut.setProtocolVersion("HTTP/1.1");
-            assertThat(sut.getProtocolVersion(), is("HTTP/1.1"));
-        }
-    }
-
-    public static class ヘッダーフィルードのテスト{
-        @Test
-        public void nullチェック () {
-            RequestMessage sut = new RequestMessage();
-            assertThat(sut.findHeaderByName(null), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            RequestMessage sut = new RequestMessage();
-            sut.setHeaderFieldUri("person", "人");
-            assertThat(sut.findHeaderByName("person"), is("人"));
-        }
-    }
-
-    public static class メッセージボディのテスト{
-        @Test
-        public void nullチェック () {
-            RequestMessage sut = new RequestMessage();
-            assertThat(sut.findMessageBody(null), is(nullValue()));
-        }
-        @Test
-        public void 正しく値を得られるか () {
-            RequestMessage sut = new RequestMessage();
-            sut.setMessageBody("person", "人");
-            assertThat(sut.findMessageBody("person"), is("人"));
-        }
-    }
-
-    public static class パースのテスト{
-        @Test
-        public void nullチェック () throws Exception{
-            RequestMessage sut = new RequestMessage();
-            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/empty.txt");
-            InputStream is = new FileInputStream(file);
-            assertThat(sut.parse(is), is(false));
-        }
-        @Test
-        public void パースのテストをしようと思う () throws Exception{
-            RequestMessage sut = new RequestMessage();
-            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/requestMessage.txt");
-            InputStream is = new FileInputStream(file);
-
-            assertThat(sut.parse(is), is(true));
-
-            //以降のテストはparseメソッド前提である
-
-            assertThat(sut.getMethod(), is("GET"));
-            assertThat(sut.getUri(),is("/index.html"));
-            assertThat(sut.getProtocolVersion(),is("HTTP/1.1"));
-            assertThat(sut.findHeaderByName("Host"),is("localhost:8080"));
-            assertThat(sut.findUriQuery("name"),is("asada"));
-            assertThat(sut.findUriQuery("like"),is("cat"));
-        }
-        @Test
-        public void HOSTでメッセージボディに何か入れてみる() throws Exception{
-            RequestMessage sut = new RequestMessage();
-            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/HostRequestMessage.txt");
-            InputStream is = new FileInputStream(file);
-
-            assertThat(sut.parse(is), is(true));
-
-            //以降のテストはparseメソッド前提である
-
-            assertThat(sut.getMethod(), is("HOST"));
-            assertThat(sut.getUri(), is("/index.html"));
-            assertThat(sut.getProtocolVersion(), is("HTTP/1.1"));
-            assertThat(sut.findHeaderByName("Host"), is("localhost:8080"));
-            assertThat(sut.findMessageBody("name"), is("asada"));
-            assertThat(sut.findMessageBody("like"), is("cat"));
-        }
-    }
+//    public static class methodのテスト{
+//        RequestMessage sut = new RequestMessage();
+//        @Before
+//        public void setUp () {
+//            sut = new RequestMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            assertThat(sut.getMethod(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            sut.setMethod("GET");
+//            assertThat(sut.getMethod(), is("GET"));
+//        }
+//    }
+//
+//    public static class URIのテスト{
+//        RequestMessage sut;
+//        @Before
+//        public void setUp () {
+//            sut = new RequestMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            assertThat(sut.getUri(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            sut.setUri("/index.html");
+//            assertThat(sut.getUri(), is("/index.html"));
+//        }
+//    }
+//
+//    public static class URIのクエリーのテスト{
+//        RequestMessage sut;
+//        @Before
+//        public void setUp () {
+//            sut = new RequestMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            assertThat(sut.findUriQuery(null), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            sut.setUriQuery("person", "人");
+//            assertThat(sut.findUriQuery("person"), is("人"));
+//        }
+//    }
+//
+//    public static class プロトコルバージョンのテスト{
+//        RequestMessage sut;
+//        @Before
+//        public void setUp () {
+//            sut = new RequestMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            assertThat(sut.getProtocolVersion(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            sut.setProtocolVersion("HTTP/1.1");
+//            assertThat(sut.getProtocolVersion(), is("HTTP/1.1"));
+//        }
+//    }
+//
+//    public static class ヘッダーフィルードのテスト{
+//        @Test
+//        public void nullチェック () {
+//            RequestMessage sut = new RequestMessage();
+//            assertThat(sut.findHeaderByName(null), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            RequestMessage sut = new RequestMessage();
+//            sut.setHeaderFieldUri("person", "人");
+//            assertThat(sut.findHeaderByName("person"), is("人"));
+//        }
+//    }
+//
+//    public static class メッセージボディのテスト{
+//        @Test
+//        public void nullチェック () {
+//            RequestMessage sut = new RequestMessage();
+//            assertThat(sut.findMessageBody(null), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しく値を得られるか () {
+//            RequestMessage sut = new RequestMessage();
+//            sut.setMessageBody("person", "人");
+//            assertThat(sut.findMessageBody("person"), is("人"));
+//        }
+//    }
+//
+//    public static class パースのテスト{
+//        @Test
+//        public void nullチェック () throws Exception{
+//            RequestMessage sut = new RequestMessage();
+//            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/empty.txt");
+//            InputStream is = new FileInputStream(file);
+//            assertThat(sut.parse(is), is(false));
+//        }
+//        @Test
+//        public void パースのテストをしようと思う () throws Exception{
+//            RequestMessage sut = new RequestMessage();
+//            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/requestMessage.txt");
+//            InputStream is = new FileInputStream(file);
+//
+//            assertThat(sut.parse(is), is(true));
+//
+//            //以降のテストはparseメソッド前提である
+//
+//            assertThat(sut.getMethod(), is("GET"));
+//            assertThat(sut.getUri(),is("/index.html"));
+//            assertThat(sut.getProtocolVersion(),is("HTTP/1.1"));
+//            assertThat(sut.findHeaderByName("Host"),is("localhost:8080"));
+//            assertThat(sut.findUriQuery("name"),is("asada"));
+//            assertThat(sut.findUriQuery("like"),is("cat"));
+//        }
+//        @Test
+//        public void HOSTでメッセージボディに何か入れてみる() throws Exception{
+//            RequestMessage sut = new RequestMessage();
+//            File file = new File("./src/test/java/jp/co/topgate/asada/web/Documents/HostRequestMessage.txt");
+//            InputStream is = new FileInputStream(file);
+//
+//            assertThat(sut.parse(is), is(true));
+//
+//            //以降のテストはparseメソッド前提である
+//
+//            assertThat(sut.getMethod(), is("HOST"));
+//            assertThat(sut.getUri(), is("/index.html"));
+//            assertThat(sut.getProtocolVersion(), is("HTTP/1.1"));
+//            assertThat(sut.findHeaderByName("Host"), is("localhost:8080"));
+//            assertThat(sut.findMessageBody("name"), is("asada"));
+//            assertThat(sut.findMessageBody("like"), is("cat"));
+//        }
+//    }
 }
--- src/test/java/jp/co/topgate/asada/web/ResponseMessageTest.java
+++ src/test/java/jp/co/topgate/asada/web/ResponseMessageTest.java
@@ -15,117 +15,117 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by yusuke-pc on 2017/04/13.
  */
-@RunWith(Enclosed.class)
+//@RunWith(Enclosed.class)
 public class ResponseMessageTest {
-    static ResponseMessage sut = null;
-
-    public static class プロトコルバージョンのテスト {
-        @Before
-        public void setUp () {
-            sut = new ResponseMessage();
-        }
-        @Test
-        public void nullチェック () {
-            sut.setProtocolVersion(null);
-            assertThat(sut.getProtocolVersion(), is(nullValue()));
-        }
-        @Test
-        public void 正しくセットできているか () {
-            sut.setProtocolVersion("HTTP/1.1");
-            assertThat("HTTP/1.1", is(sut.getProtocolVersion()));
-        }
-    }
-
-    public static class ステータスコードのテスト {
-        @Before
-        public void setUp () {
-            sut = new ResponseMessage();
-        }
-        @Test
-        public void nullチェック () {
-            sut.setStatusCode(null);
-            assertThat(sut.getStatusCode(), is(nullValue()));
-        }
-        @Test
-        public void 正しくセットできているか () {
-            sut.setStatusCode("200");
-            assertThat(sut.getStatusCode(), is("200"));
-        }
-    }
-
-    public static class 理由フレーズのテスト {
-        @Before
-        public void setUp () {
-            sut = new ResponseMessage();
-        }
-        @Test
-        public void nullチェック () {
-            sut.setReasonPhrase(null);
-            assertThat(sut.getReasonPhrase(), is(nullValue()));
-        }
-        @Test
-        public void 正しくセットできているか () {
-            sut.setReasonPhrase("OK");
-            assertThat(sut.getReasonPhrase(), is("OK"));
-        }
-    }
-
-    public static class ヘッダーボディのテスト {
-        @Before
-        public void setUp () {
-            sut = new ResponseMessage();
-        }
-        @Test
-        public void 両方null () {
-            sut.addHeader(null,null);
-            assertThat(sut.getHeaderField().size(), is(0));
-        }
-        @Test
-        public void nameをnull () {
-            sut.addHeader(null,"value");
-            assertThat(sut.getHeaderField().size(), is(0));
-        }
-        @Test
-        public void valueをnull () {
-            sut.addHeader("name",null);
-            assertThat(sut.getHeaderField().size(), is(0));
-        }
-        @Test
-        public void 正しくセットできているか (){
-            sut.addHeader("Date", "Thu,13 Api 2017 18:33:23 GMT");
-            sut.addHeader("Server", "mywebserver/1.0");
-            assertThat(sut.getHeaderField().get(0), is("Date: Thu,13 Api 2017 18:33:23 GMT"));
-            assertThat(sut.getHeaderField().get(1), is("Server: mywebserver/1.0"));
-        }
-    }
-
-    public static class メッセージボディのテスト {
-        @Before
-        public void setUp () {
-            sut = new ResponseMessage();
-        }
-        @Test
-        public void nullチェック () {
-            sut.setMessageBody(null);
-            assertThat(sut.getMessageBody(), is(nullValue()));
-        }
-        @Test
-        public void 存在しないファイルを渡してみるnullのままのはず () {
-            sut.setMessageBody(new File("./Documents/notExists"));
-            assertThat(sut.getMessageBody(), is(nullValue()));
-        }
-        @Test
-        public void ディレクトリのパスを渡してみるnullのままのはず () {
-            sut.setMessageBody(new File("./src/test/java/jp/co/topgate/asada/web/Documents"));
-            assertThat(sut.getMessageBody(), is(nullValue()));
-        }
-        @Test
-        public void 正しくセットできているか () {
-            sut.setMessageBody(
-                    new File("./src/test/java/jp/co/topgate/asada/web/Documents/requestMessage.txt"));
-            assertThat(sut.getMessageBody(), is(notNullValue()));
-        }
-    }
+//    static ResponseMessage sut = null;
+//
+//    public static class プロトコルバージョンのテスト {
+//        @Before
+//        public void setUp () {
+//            sut = new ResponseMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            sut.setProtocolVersion(null);
+//            assertThat(sut.getProtocolVersion(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しくセットできているか () {
+//            sut.setProtocolVersion("HTTP/1.1");
+//            assertThat("HTTP/1.1", is(sut.getProtocolVersion()));
+//        }
+//    }
+//
+//    public static class ステータスコードのテスト {
+//        @Before
+//        public void setUp () {
+//            sut = new ResponseMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            sut.setStatusCode(null);
+//            assertThat(sut.getStatusCode(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しくセットできているか () {
+//            sut.setStatusCode("200");
+//            assertThat(sut.getStatusCode(), is("200"));
+//        }
+//    }
+//
+//    public static class 理由フレーズのテスト {
+//        @Before
+//        public void setUp () {
+//            sut = new ResponseMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            sut.setReasonPhrase(null);
+//            assertThat(sut.getReasonPhrase(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しくセットできているか () {
+//            sut.setReasonPhrase("OK");
+//            assertThat(sut.getReasonPhrase(), is("OK"));
+//        }
+//    }
+//
+//    public static class ヘッダーボディのテスト {
+//        @Before
+//        public void setUp () {
+//            sut = new ResponseMessage();
+//        }
+//        @Test
+//        public void 両方null () {
+//            sut.addHeader(null,null);
+//            assertThat(sut.getHeaderField().size(), is(0));
+//        }
+//        @Test
+//        public void nameをnull () {
+//            sut.addHeader(null,"value");
+//            assertThat(sut.getHeaderField().size(), is(0));
+//        }
+//        @Test
+//        public void valueをnull () {
+//            sut.addHeader("name",null);
+//            assertThat(sut.getHeaderField().size(), is(0));
+//        }
+//        @Test
+//        public void 正しくセットできているか (){
+//            sut.addHeader("Date", "Thu,13 Api 2017 18:33:23 GMT");
+//            sut.addHeader("Server", "mywebserver/1.0");
+//            assertThat(sut.getHeaderField().get(0), is("Date: Thu,13 Api 2017 18:33:23 GMT"));
+//            assertThat(sut.getHeaderField().get(1), is("Server: mywebserver/1.0"));
+//        }
+//    }
+//
+//    public static class メッセージボディのテスト {
+//        @Before
+//        public void setUp () {
+//            sut = new ResponseMessage();
+//        }
+//        @Test
+//        public void nullチェック () {
+//            sut.setMessageBody(null);
+//            assertThat(sut.getMessageBody(), is(nullValue()));
+//        }
+//        @Test
+//        public void 存在しないファイルを渡してみるnullのままのはず () {
+//            sut.setMessageBody(new File("./Documents/notExists"));
+//            assertThat(sut.getMessageBody(), is(nullValue()));
+//        }
+//        @Test
+//        public void ディレクトリのパスを渡してみるnullのままのはず () {
+//            sut.setMessageBody(new File("./src/test/java/jp/co/topgate/asada/web/Documents"));
+//            assertThat(sut.getMessageBody(), is(nullValue()));
+//        }
+//        @Test
+//        public void 正しくセットできているか () {
+//            sut.setMessageBody(
+//                    new File("./src/test/java/jp/co/topgate/asada/web/Documents/requestMessage.txt"));
+//            assertThat(sut.getMessageBody(), is(notNullValue()));
+//        }
+//    }
 
 //    public static class レスポンスメッセージの生成テスト {
 //        @Before
