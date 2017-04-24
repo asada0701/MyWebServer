99f34e236a6b7b2fb2b3ca17e70e424c8b52d33f
asada0701 <yusuke.nakashima@topgate.co.jp>
2017-04-24 22:25:43 +0900

    ResourceFileテストの修正

--- src/test/java/jp/co/topgate/asada/web/ResourceFileTest.java
+++ src/test/java/jp/co/topgate/asada/web/ResourceFileTest.java
@@ -13,9 +13,10 @@ import static org.junit.Assert.assertThat;
 @RunWith(Enclosed.class)
 public class ResourceFileTest {
     public static class コンストラクタのテスト {
-        @Test(expected = NullPointerException.class)
+        @Test
         public void nullチェック() {
             ResourceFile sut = new ResourceFile(null);
+            assertThat(sut.getContentType(), is("application/octet-stream"));
         }
 
         @Test
